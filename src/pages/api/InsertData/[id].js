c2b7c86e2e0534e7caf8bf2d7fad1760b3a870c3
Robert Leon <team@platzi.com>
2023-05-30 15:17:58 -0500

    End Points

---
+++ src/pages/api/InsertData/[id].js
@@ -0,0 +1,28 @@
+import prisma from '@/prisma/prisma';
+
+
+export default async function handler(req, res) {
+    // res.status(200).json({ name: 'John Doe' })
+
+    const { method } = req;
+    const { id } = req.query
+    switch (method) {
+        case 'GET':
+            try {
+                const product = await prisma.food.findUnique({
+                    where: { id: parseInt(id) }
+                });
+                return res.status(200).json({ product })
+
+            } catch (error) {
+                res.status(500).json({ error: error.message })
+            }
+
+
+
+        default:
+            res.status(500).json({ error: "fallo" })
+
+            break;
+    }
+} 
