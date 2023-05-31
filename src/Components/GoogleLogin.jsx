3ba963188d1d3106e9ca3b9c98f8d77fe04b59c5
lulilcm <lourdescandelamoreno@gmail.com>
2023-05-31 16:51:20 -0300

    GoogleLogin component
    creación de un componente para guardar la lógica

---
+++ src/Components/GoogleLogin.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const GoogleLogin = () => {
+    const { data: session, status } = useSession();
+    if (status === 'authenticated') {
+      return (
+        <>
+          Bienvenido/a  {session.user.name} <br />
+          <button onClick={() => signOut()}>Log out</button>
+        </>
+      )
+    }
+    return (
+      <>
+        Registrarse con: <br />
+        <button onClick={() => signIn()}>Google</button>
+      </>
+    )
+};
+
+export default GoogleLogin;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,22 +1,11 @@
+import GoogleLogin from '@/Components/GoogleLogin';
 import React from 'react';
-import { useSession, signIn, signOut } from "next-auth/react"
+
 
 const Login = () => {
-  const { data: session, status } = useSession();
-  if (status === 'authenticated') {
-    return (
-      <>
-        Bienvenido/a  {session.user.name} <br />
-        <button onClick={() => signOut()}>Log out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Registrarse con: <br />
-      <button onClick={() => signIn()}>Google</button>
-    </>
-  )
+  return(
+    <GoogleLogin />
+  );
 };
 
 
